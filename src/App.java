4ddda48791d4c92264dbdee9bfd45f629e2e8d8e
DanielCesurMalaga <daniel.fernandezcampos@cesurformacion.com>
2024-10-07 12:25:47 +0200

    terminado trabajo con BufferedReader

--- src/App.java
+++ src/App.java
@@ -1,10 +1,12 @@
 import java.io.IOException;
-import clases.FlujoAleatorio;
+
+import clases.AccesoBuffer;
+
 
 
 public class App {
     public static void main(String[] args) throws IOException {
-        
+        AccesoBuffer.inicio();
 
     }
 }
--- src/clases/AccesoBuffer.java
+++ src/clases/AccesoBuffer.java
@@ -1,19 +1,61 @@
 package clases;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
-
+import java.io.IOException;
 
 public class AccesoBuffer {
-    public static void inicio(){
-
+    public static void inicio() throws IOException {
+        mostrarYcontarContenido("texto.txt");
     }
 
-    private static boolean mostrarContenido(String fichero){
+    private static boolean mostrarContenido(String nombreFichero) throws IOException {
 
+        BufferedReader lector = null;
+
+        try {
+            lector = new BufferedReader(new FileReader(nombreFichero));
+            String frase = lector.readLine();
+            while (frase != null) {
+                System.out.println(frase);
+                frase = lector.readLine();
+            }
+            return true;
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+            return false;
+        } finally {
+            if (lector != null)
+                lector.close();
+        }
     }
 
-    private static boolean mostrarYcontarContenido(String fichero){
-        
+    private static boolean mostrarYcontarContenido(String fichero) throws IOException {
+        // contar significa contar las líneas totales y cuantas vacías hay.
+        BufferedReader lector = null;
+        int numLineas=0;
+        int numVacias=0;
+
+        try {
+            lector = new BufferedReader(new FileReader("texto.txt"));
+            String frase = lector.readLine();
+            while (frase != null) {
+                numLineas++;
+                if (frase.isEmpty()) numVacias++;
+                System.out.println(frase);
+                frase = lector.readLine();
+            }
+            System.out.println("Líneas totales: "+ numLineas);
+            System.out.println("Líneas vacías: "+ numVacias);
+            return true;
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            if (lector != null)
+                lector.close();
+        }
+        return true;
     }
 }
